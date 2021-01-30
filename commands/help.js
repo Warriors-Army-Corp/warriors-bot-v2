0d28fdac2ed4a5bb54dd91c911c202f155aa4cde
WarriorOfMeadow <39090431+WarriorOfMeadow@users.noreply.github.com>
2021-01-30 15:40:48 +0100

    Amélioration de l'embed du "help".

--- commands/help.js
+++ commands/help.js
@@ -2,11 +2,17 @@ const { MessageEmbed, Collection } = require('discord.js');
 
 exports.cmd = (client, msg) => {
 
-  const helpEmbed = new MessageEmbed()
-    .setTitle(`LISTE DES COMMANDES | prefix : ${client.PREFIX}`)
-    .setColor(msg.member.roles.highest.color)
-    .setFooter(`${client.MARQUE}`, client.THUMB)
-    .setThumbnail("https://cdn.discordapp.com/emojis/395627468276367370.png?v=1")
+  const helpEmbed = new MessageEmbed({
+    "title": `LISTE DES COMMANDES | prefix : ${client.PREFIX}`,
+    "color": msg.member.roles.highest.color,
+    "thumbnail": {
+      "url": "https://cdn.discordapp.com/emojis/395627468276367370.png?v=1"
+    },
+    "footer": {
+      "icon_url": client.THUMB,
+      "text": `${client.MARQUE}`
+    }
+  });
 
   client.commands.forEach(help => {
     if (msg.member.hasPermission(help.help.perm)) {
