2c3ad37922bae703d7f88dfb185c1197b8873be6
WarriorOfMeadow <39090431+WarriorOfMeadow@users.noreply.github.com>
2021-01-31 18:17:32 +0100

    heu... chépo

--- commands/ban.js
+++ commands/ban.js
@@ -10,25 +10,29 @@ exports.cmd = async (client, msg, args) => {
     if (user) {
       const member = msg.guild.member(user);
       if (member) {
-        const victime = user.tag;
-        args.shift();
-        const reason = args.join(" ");
-        await member.createDM().then(ch => ch.send(`Vous avez été frappé par le banhammer, asséné part ${msg.author.tag}, pour la raison : "${reason}".`).catch(hook.send("Je n'est pas pu envoyer de message à la personne banni :/")));
-        await member.ban(`${msg.author.tag} a banni ${victime} pour la raison : ${reason}`)
-        await msg.channel.send(`${victime} s'est prit un violant coup de banhammer sur la tête de la part de ${msg.author}`);
-        await hook.send(`${msg.author} a banni ${victime} pour la raison : "${reason}"`);
-        /*const embed = new RichEmbed({
-          "embeds": [
-            {
-              "title": "Nouveau banni",
-              "color": 1,
-              "image": {
-                "url": "https://media.discordapp.net/attachments/664493039410085899/741425878759768064/telechargement.jpg"
+        if (member.bannable) {
+          const victime = user.tag;
+          args.shift();
+          const reason = args.join(" ");
+          await member.createDM().then(ch => ch.send(`Vous avez été frappé par le banhammer, asséné part ${msg.author.tag}, pour la raison : "${reason}".`)).catch(msg.reply("Je n'est pas pu envoyer de message à la personne banni :/"))await js;
+          await member.ban(`${msg.author.tag} a banni ${victime} pour la raison : ${reason}`)
+          await msg.channel.send(`${victime} s'est prit un violant coup de banhammer sur la tête de la part de ${msg.author}`);
+          await hook.send(`${msg.author} a banni ${victime} pour la raison : "${reason}"`);
+          /*const embed = new RichEmbed({
+            "embeds": [
+              {
+                "title": "Nouveau banni",
+                "color": 1,
+                "image": {
+                  "url": "https://media.discordapp.net/attachments/664493039410085899/741425878759768064/telechargement.jpg"
+                }
               }
-            }
-          ]
-        }).setDescription(`${msg.author.tag} a banni ${victime} pour la raison : "${reason}"`).setAuthor(`${msg.author.tag}`, `${msg.author.avatarURL}`)*/
-        //hook.send(embed);
+            ]
+          }).setDescription(`${msg.author.tag} a banni ${victime} pour la raison : "${reason}"`).setAuthor(`${msg.author.tag}`, `${msg.author.avatarURL}`)*/
+          //hook.send(embed);
+        } else {
+          msg.reply("J'peux pas l'ban 😑");
+        }
       } else {
         msg.reply("J'le trouve pas sur le serv celui là 🤔");
       }
