afdc5d32c37f2e9f25e18c5f6215354c847dd367
Nemo-Brunschwig <nemo.brunschwig@gmail.com>
2021-04-03 22:29:30 +0200

    Amélioration de la roulette.

--- commands/roulette.js
+++ commands/roulette.js
@@ -1,21 +1,18 @@
-exports.cmd = (client, msg) => {
-  if(msg.guild.id === "585906194724552706"){
+exports.cmd = async (client, msg) => {
+  if(msg.guild.id === "645239930896908293"){
     var player = msg.member;
     if (player.kickable && !player.roles.cache.find(rl => rl.id == "649718517310029874")) {
       var roll = Math.floor(Math.random() * (client.ROULETTE)) + 1;
       if (roll === 1) {
-        (async () => {
-          await msg.author.createDM().then(dm => dm.send("Vous avez joué, vous avez perdu :shrug: vous pouvez quand même revenir https://discord.gg/N49Gxsu").catch());
-          await player.kick("A perdu à la Roulette Russe").catch();
-          await msg.channel.send("Ah! il a perdu :frowning:")
-          client.ROULETTE = 6;
-        })();
+        await msg.author.createDM().then(dm => dm.send("Vous avez joué, vous avez perdu :shrug: vous pouvez quand même revenir https://discord.gg/N49Gxsu").catch());
+        await player.kick("A perdu à la Roulette Russe").catch();
+        await msg.channel.send("Ah! il a perdu :frowning:")
+        await (client.ROULETTE = 6);
       } else {
-        msg.channel.send("C'est pas passé loin :hot_face:");
-        client.ROULETTE--;
+        await msg.channel.send("C'est pas passé loin :hot_face:");
+        await (client.ROULETTE--);
       }
-      console.log(client.ROULETTE + 1);
-      console.log(roll);
+      await console.log(`coups restant : ${client.ROULETTE}`);
     } else {
       msg.reply("Je ne peux pas vous kick donc vous ne pouvez pas jouer... désolé Ô grand maître :person_bowing:")
     }
