ef1484167bd2182d1da2304f634f67acb78f5422
Mizari-W <warriorofmeadow@gmail.com>
2021-04-12 01:48:32 +0200

    modification de la taille d'image pour la commande pp

--- commands/pp.js
+++ commands/pp.js
@@ -3,11 +3,11 @@ exports.cmd = /*async si besoin*/ (client, msg, args) => {
   // si y a une mention on affiche la pp de la personne mentionnée
   if (user) {
     // si la pp est un gif, elle sera animé. "?size=1280" permet de redimentionné au format le plus grand
-    msg.channel.send(user.avatarURL({dynamic: true})+"?size=1280");
+    msg.channel.send(user.avatarURL({dynamic: true})+"?size=4096");
   // si y a pas de mention on affiche la pp de l'auteur du message
   } else {
     // si la pp est un gif, elle sera animé. "?size=1280" permet de redimentionné au format le plus grand
-    msg.channel.send(msg.author.avatarURL({dynamic: true})+"?size=1280");
+    msg.channel.send(msg.author.avatarURL({dynamic: true})+"?size=4096");
   }
 }
 
