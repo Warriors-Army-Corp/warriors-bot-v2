87b7926dde35c84c085150da47d404c73c28c277
Mizari-W <warriorofmeadow@gmail.com>
2021-04-12 04:30:12 +0200

    ajout d'un commentaire

--- fonctions/stringToHex.js
+++ fonctions/stringToHex.js
@@ -1,3 +1,4 @@
+// fonction pour renvoyer l'hexa d'un caractère unicod (c'était pour un test mais je la garde au cas ou)
 module.exports = function stringToHex(str) {
   res = [];
   for (var i = 0; i < str.length; i++) {
