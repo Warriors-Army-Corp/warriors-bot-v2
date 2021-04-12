989c16b7a7012147948b52520b8ae1e199dcdcf6
Mizari-W <warriorofmeadow@gmail.com>
2021-04-12 04:47:14 +0200

    ça y est ! la roulette est publique ! 🥳

--- commands/roulette.js
+++ commands/roulette.js
@@ -1,23 +1,42 @@
+// appel de la fonction pour avoir une invite
+const createInvite = require('../fonctions/createInvite.js');
+
 exports.cmd = async (client, msg) => {
-  if(msg.guild.id === "645239930896908293"){
-    var player = msg.member;
-    if (player.kickable && !player.roles.cache.find(rl => rl.id == "649718517310029874")) {
-      var roll = Math.floor(Math.random() * (client.ROULETTE)) + 1;
-      if (roll === 1) {
-        await msg.author.createDM().then(dm => dm.send("Vous avez joué, vous avez perdu :shrug: vous pouvez quand même revenir https://discord.gg/N49Gxsu").catch());
-        await player.kick("A perdu à la Roulette Russe").catch();
-        await msg.channel.send("Ah! il a perdu :frowning:")
-        await (client.ROULETTE = 6);
-      } else {
-        await msg.channel.send("C'est pas passé loin :hot_face:");
-        await (client.ROULETTE--);
-      }
-      await console.log(`coups restant : ${client.ROULETTE}`);
+  var guild = msg.guild; // on récupère le serv
+  var player = msg.member; // on récupère le joueur
+
+  // si le serv n'a pas la propriété "roulette"
+  if (!guild.roulette) {
+    // on la créée et on l'initialise à 6
+    guild.roulette = 6;
+  }
+
+  // si le joueur est kickable et qu'il ne boost pas le serv
+  if (player.kickable && player.premiumSinceTimestamp === 0) {
+    // on choisi un nombre au hasard entre 1 (inclu) et le nombre de coups restants (inclu)
+    var roll = Math.floor(Math.random() * (guild.roulette)) + 1;
+    // si le nombre est égale à 1 (correspond à la balle)
+    if (roll === 1) {
+      // on récupère une invite
+      const invite = await createInvite(msg);
+      // on envoit un message en MP au perdant avec l'invite
+      await msg.author.createDM().then(dm => dm.send("Vous avez joué, vous avez perdu :shrug: vous pouvez quand même revenir https://discord.gg/"+invite).catch());
+      // on kick le membre (c'est le jeu ma pauvre Lucette)
+      await player.kick("A perdu à la Roulette Russe").catch();
+      // on envoit un message dans le salon où s'est déroulé le jeu
+      await msg.channel.send("Ah! il a perdu :frowning:")
+      // on réinitialise la roulette à 6
+      await (guild.roulette = 6);
+    // si le joueur ne s'est pas prit la balle
     } else {
-      msg.reply("Je ne peux pas vous kick donc vous ne pouvez pas jouer... désolé Ô grand maître :person_bowing:")
+      // on envoit un message dans le salon où se déroule le jeu pour dire qu'il a eu chaud
+      await msg.channel.send("C'est pas passé loin :hot_face:");
+      // on décrémente le nombre de coups
+      await (guild.roulette--);
     }
   } else {
-    msg.reply("Pas encore disponible désolé :/");
+    // si la personne n'est pas kickable ou est booster on s'excuse pour le désagrément
+    msg.reply("Je ne peux pas vous kick donc vous ne pouvez pas jouer... désolé Ô grand maître :person_bowing:")
   }
 }
 
