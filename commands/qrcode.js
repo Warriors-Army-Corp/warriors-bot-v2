c9524ef733bc6457fe5d522ac8eb50d6a5b17f2c
Mizari-W <warriorofmeadow@gmail.com>
2021-04-14 01:44:44 +0200

    création de la commande QR code

---
+++ commands/qrcode.js
@@ -0,0 +1,27 @@
+const QRCode = require('qrcode');
+const tempy = require('tempy');
+
+exports.cmd = async (client, msg, args) => {
+  if (args.length>0) {
+    const output = tempy.file({extension: 'png'});
+
+    QRCode.toFile(output, args.join(" "), {margin: 1}, function (error) {
+      if (error) console.error(error);
+      msg.channel.send({
+        files: [{
+          attachment: output,
+          name: 'qrcode.png'
+        }]
+      }).catch();
+    });
+  }else {
+    msg.channel.send("Il me faut du texte svp 👀");
+  }
+}
+
+exports.help = {
+  perm: "SEND_MESSAGES",
+  cmd: "qrcode",
+  args: "[un texte quelconque]",
+  desc: "Permet de transformer votre texte en image QR code."
+}
