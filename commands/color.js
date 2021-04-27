5ab3823414bce22f0a52df10e848ed46ed4afc26
Mizari-W <warriorofmeadow@gmail.com>
2021-04-27 09:37:42 +0200

    amélioration de la commande "couleurinfo"

--- commands/color.js
+++ commands/color.js
@@ -11,16 +11,21 @@ exports.cmd = async (client, msg, args) => {
     var colorEmbed = new MessageEmbed({
       "title": "Infos sur la couleur "+resp.hex.value,
       "color": resp.hex.value,
-      "description": `niveau de rouge : ${resp.rgb.r}
+      "description": `**RVB**
+        niveau de rouge : ${resp.rgb.r}
         niveau de vert : ${resp.rgb.g}
         niveau de bleu : ${resp.rgb.b}
-        niveau de teinte : ${resp.hsl.h}
-        niveau de saturation : ${resp.hsl.s}%
-        niveau de lumière : ${resp.hsl.l}%
+        -----------------------------
+        **CMYK**
         niveau de cyan : ${resp.cmyk.c}
         niveau de magenta : ${resp.cmyk.m}
         niveau de jaune : ${resp.cmyk.y}
-        niveau de noir : ${resp.cmyk.k}`,
+        niveau de noir : ${resp.cmyk.k}
+        -----------------------------
+        **HSL**
+        niveau de teinte : ${resp.hsl.h}
+        niveau de saturation : ${resp.hsl.s}%
+        niveau d'exposition' : ${resp.hsl.l}%`,
       "image": {
         "url": `https://singlecolorimage.com/get/${resp.hex.clean}/200x100`
       }
