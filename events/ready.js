3783e8e78feabaef7a8bffd11c355521c657e276
Mizari-W <warriorofmeadow@gmail.com>
2021-06-08 16:42:34 +0200

    vérification des serv dans la db

--- events/ready.js
+++ events/ready.js
@@ -1,41 +1,44 @@
 const fetch = require('node-fetch');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
+});
 
 module.exports = (client) => {
   console.log(`Logged in as ${client.user.tag}!`); //affichage dans la console que le client est bien co (c'est pour Mizari)
+  client.user.setActivity(`${client.guilds.cache.size} servers`, {type: 'COMPETING'}); //affiche sur combien de serv est le bot
 
-  var count = 0;
-  setInterval(async () => {
-    if(count > 4)
-      count = 0;
-    switch (count) {
-      case 0:
-        var followers = await fetch("https://api.twitch.tv/kraken/channels/440161668/follows?limit=1", {
-          method: "get",
-          headers: {
-            "Client-ID": process.env.TWITCH_ID,
-            "Accept": "application/vnd.twitchtv.v5+json"
-          }
-        }).then(res => res.json()).then(json => json._total).catch();
-        client.user.setActivity(`${followers} followers | WarriorsArmyOff on Twitch`, {type: 'STREAMING', url: "https://www.twitch.tv/warriorsarmyoff"});
-        break;
-      case 1:
-        var subscribers = await fetch("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCwlbtfVO3ilb1g1_ZcBhwnQ&key="+process.env.YOUTUBE_KEY).then(res => res.json()).then(json => json.items[0].statistics.subscriberCount).catch();
-        client.user.setActivity(`${subscribers} subscribers | Warriors Army on YouTube`, {type: 'WATCHING'});
-        break;
-      case 2:
-        client.user.setActivity(`${client.PREFIX}help | ${client.MARQUE}`, {type: 'PLAYING'});
-        break;
-      case 3:
-        client.user.setActivity("WAC song", {type: 'LISTENING'});
-        break;
-      case 4:
-        client.user.setActivity(`${client.guilds.cache.size} servers`, {type: 'COMPETING'});
-        break;
-      default:
-        console.log("y a un problème chef");
-        client.user.setActivity("error", {type: 'PLAYING'});
+  connection.connect(function(err) {
+    if (err) {
+      console.error("connection à la DB fail : "+err);
+      return;
     }
-    count++;
-  }, 10000);
+    console.log("connection à la DB réussi");
+  });
+
+  client.guilds.cache.each(guild => {
+    var id = guild.id;
+    connection.query(`SELECT id FROM guild WHERE id=${id}`, function(err, results){
+      if (err) {
+        console.error("J'arrive pas à chercher le serveur "+id+" : "+err);
+        return;
+      }
+
+      if(results.length === 0){
+        connection.query(`INSERT INTO guild (id) VALUES('${id}')`, function(err){
+          if (err) {
+            console.error(`J'arrive pas à insérer le serveur ${id} : ${err}`);
+            return;
+          }
+
+          console.log("Insertion réussi");
+        })
+      }
+    });
+  });
 
 }
