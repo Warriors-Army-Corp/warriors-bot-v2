5e27341ce5c1ed4182f4f71a91bc669bee639678
Mizari-W <warriorofmeadow@gmail.com>
2021-06-08 18:33:10 +0200

    amélioration du ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,44 +1,13 @@
 const fetch = require('node-fetch');
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE
-});
+const checkGuild = require('../fonctions/checkGuildDB.js');
 
 module.exports = (client) => {
   console.log(`Logged in as ${client.user.tag}!`); //affichage dans la console que le client est bien co (c'est pour Mizari)
   client.user.setActivity(`${client.guilds.cache.size} servers`, {type: 'COMPETING'}); //affiche sur combien de serv est le bot
 
-  connection.connect(function(err) {
-    if (err) {
-      console.error("connection à la DB fail : "+err);
-      return;
-    }
-    console.log("connection à la DB réussi");
-  });
-
   client.guilds.cache.each(guild => {
     var id = guild.id;
-    connection.query(`SELECT id FROM guild WHERE id=${id}`, function(err, results){
-      if (err) {
-        console.error("J'arrive pas à chercher le serveur "+id+" : "+err);
-        return;
-      }
-
-      if(results.length === 0){
-        connection.query(`INSERT INTO guild (id) VALUES('${id}')`, function(err){
-          if (err) {
-            console.error(`J'arrive pas à insérer le serveur ${id} : ${err}`);
-            return;
-          }
-
-          console.log("Insertion réussi");
-        })
-      }
-    });
+    checkGuild(id);
   });
 
 }
