03d0b86b6791eb4bab0d17c9e2a53441f6173eda
Mizari-W <warriorofmeadow@gmail.com>
2021-06-09 15:28:47 +0200

    fonction pour se co à la db

---
+++ fonctions/connectDB.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
+});
+
+module.exports = function connectDB(){
+  connection.connect(function(err) {
+    if (err) {
+      console.error("connection à la DB fail : "+err);
+      return;
+    }
+    console.log("connection à la DB réussi");
+  });
+
+  return connect;
+}
