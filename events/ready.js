0ba7d4847fb7c8ddb93cd96fb6ce2e1ad78ec5d5
Mizari-W <warriorofmeadow@gmail.com>
2021-07-21 13:40:41 +0200

    amélioration du ready pour les slashcommands + optimisation RAM

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,10 @@
 const fetch = require('node-fetch');
 const checkGuild = require('../fonctions/checkGuildDB.js');
 
-module.exports = (client) => {
+module.exports = async (client) => {
   console.log(`Logged in as ${client.user.tag}!`); //affichage dans la console que le client est bien co (c'est pour Mizari)
   client.user.setActivity(`${client.guilds.cache.size} servers | ${client.PREFIX}help`, {type: 'COMPETING'}); //affiche sur combien de serv est le bot
 
+  await client.application.commands.set(client.arraySlashCommands);
+  //guilds.cache.get("645239930896908293")
 }
