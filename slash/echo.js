bbd536501d48edd03f9d2eacc579771a25ca8d30
Mizari-W <warriorofmeadow@gmail.com>
2021-07-21 18:09:59 +0200

    ajout de slashcommands "echo"

---
+++ slash/echo.js
@@ -0,0 +1,20 @@
+module.exports = {
+  name: "echo",
+  description: "Repeat what you want",
+  options: [
+    {
+      name: "message",
+      description: "The message you want to repeat",
+      type: "STRING",
+      required: true
+    }
+  ],
+  run: async(client, interaction, args) => {
+    if (interaction.channel.permissionsFor(interaction.guild.members.resolve(client.user)).has("SEND_MESSAGES")) {
+      interaction.deleteReply();
+      interaction.channel.send(args[0]);
+    } else {
+      interaction.followUp({ content: "Je n'ai pas la permission d'envoyer des messages ici" });
+    }
+  }
+}
