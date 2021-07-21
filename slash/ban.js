0d9f589d6e23350ee24652f5e8e9b1ffd78922a5
Mizari-W <warriorofmeadow@gmail.com>
2021-07-21 14:39:52 +0200

    amélioration de la slashcommand ban

--- slash/ban.js
+++ slash/ban.js
@@ -16,31 +16,37 @@ module.exports = {
     }
   ],
   run: async(client, interaction, args) => {
-    // déjà faut vérifier que la personne a les permissions de ban
-    if (interaction.guild.members.resolve(interaction.user).permissions.has("BAN_MEMBERS")) {
-      const userID = args[0]; // l'ID du membre à ban
-      const mbr = interaction.guild.members.cache.get(userID); // le membre à ban
-      // maintenant on vérifie que la victime est bannable
-      if (mbr.bannable) {
-        var reason = "none"; // initialisation de la raison
-        // si une raison est donnée on la stock dans "reason"
-        if (args.length > 1) {
-          reason = args[1];
-        }
+    // vérification que le bot a la perm de ban
+    if (interaction.guild.members.resolve(client.user).permissions.has("BAN_MEMBERS")) {
+      // vérification que la personne a les permissions de ban
+      if (interaction.guild.members.resolve(interaction.user).permissions.has("BAN_MEMBERS")) {
+        const userID = args[0]; // l'ID du membre à ban
+        const mbr = interaction.guild.members.cache.get(userID); // le membre à ban
+        // maintenant on vérifie que la victime est bannable
+        if (mbr && mbr.bannable) {
+          var reason = "none"; // initialisation de la raison
+          // si une raison est donnée on la stock dans "reason"
+          if (args.length > 1) {
+            reason = args[1];
+          }
 
-        // on envoit un message MP à la victime
-        await mbr.createDM().then(ch => ch.send(`Vous avez été banni de ${interaction.guild.name} par ${interaction.user.tag} pour la raison "${reason}"`)).catch();
-        // on indique que la personne s'est fait ban et on donne l'auteur du ban
-        await interaction.followUp({ content: `${mbr.user.tag} s'est prit un violent coup de banhammer sur la tête de la part de ${interaction.user}` });
-        // on ban la victime
-        await mbr.ban({ reason: `"${reason}" par ${interaction.user.tag}` }).catch();
-      // si la victime n'est pas bannable
+          // on envoit un message MP à la victime
+          await mbr.createDM().then(ch => ch.send(`Vous avez été banni de ${interaction.guild.name} par ${interaction.user.tag} pour la raison "${reason}"`)).catch();
+          // on indique que la personne s'est fait ban et on donne l'auteur du ban
+          await interaction.followUp({ content: `${mbr.user.tag} s'est prit un violent coup de banhammer sur la tête de la part de ${interaction.user}` });
+          // on ban la victime
+          await mbr.ban({ reason: `"${reason}" par ${interaction.user.tag}` }).catch();
+        // si la victime n'est pas bannable
+        } else {
+          interaction.followUp({ content : "Je ne peux pas bannir cette personne" });
+        }
+      // si la personne n'a pas la perm de ban
       } else {
-        interaction.followUp({ content : "Je ne peux pas bannir cette personne" });
+        interaction.followUp({ content: "att att... takru tavé la perm ? aaaaaah jui mort ! eh les gars ! c'bouffon a cru il pouvait m'utiliser pour ban mdr jpp" });
       }
-    // si la personne n'a pas la perm de ban
+    // si le bot a pas la perm de ban
     } else {
-      interaction.followUp({ content: "att att... takru tavé la perm ? aaaaaah jui mort ! eh les gars ! c'bouffon a cru il pouvait m'utiliser pour ban mdr jpp" });
+      interaction.followUp({ content: "J'ai pas la perm de ban..." });
     }
   }
 }
