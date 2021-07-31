3230dfea4a81a46fd95fe3a996f431250906e905
Mizari-W <warriorofmeadow@gmail.com>
2021-07-31 21:31:19 +0200

    modification de l'affichage dans la console pour le ready

--- events/ready.js
+++ events/ready.js
@@ -3,9 +3,9 @@ const checkGuild = require('../fonctions/checkGuildDB.js');
 const colors = require('../fonctions/colors.js');
 
 module.exports = async (client) => {
-  console.log(`[${colors.FgCyan}Connected ${colors.Reset}] Logged in as ${client.user.tag}!`); //affichage dans la console que le client est bien co (c'est pour Mizari)
+  console.log(`[${colors.FgCyan} Connected  ${colors.Reset}] Logged in as ${client.user.tag}!`); //affichage dans la console que le client est bien co (c'est pour Mizari)
   client.user.setActivity(`${client.guilds.cache.size} servers | ${client.PREFIX}help`, {type: 'COMPETING'}); //affiche sur combien de serv est le bot
 
-  await client.guilds.cache.get("645239930896908293").commands.set(client.arraySlashCommands);
+  await client.application.commands.set(client.arraySlashCommands);
   //guilds.cache.get("645239930896908293")
 }
