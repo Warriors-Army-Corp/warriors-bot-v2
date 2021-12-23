e23fef8b09fd561f834ab262d5a6dbcf32d1e4f8
Mizari-W <warriorofmeadow@gmail.com>
2021-12-23 16:35:10 +0100

    on repart de 0

--- events/guildCreate.js
+++
@@ -1,9 +0,0 @@
-const colors = require('../fonctions/colors.js');
-
-module.exports = (client, guild) => {
-  client.user.setActivity(`${client.guilds.cache.size} servers |  use "/"`, {type: 'COMPETING'});
-  // mise en page des logs dans la console (ça c'est pour Mizari)
-  console.log(`[${colors.FgGreen}    Logs    ${colors.Reset}] Nouveau serv : ${guild.name}`);
-  console.log(`               Nombre de membres : ${guild.memberCount}`);
-  console.log(`               Owner : ${guild.members.cache.get(guild.ownerId).user.username}`);
-}
