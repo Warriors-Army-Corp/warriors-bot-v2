9182f194fc10a3781908825bd4617892a1fa2440
Mizari-W <warriorofmeadow@gmail.com>
2021-12-24 16:39:52 +0100

    code pour delete toutes les commandes

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,19 @@
 const client = require("../index");
 const colors = require('../fonctions/colors.js');
 
-client.on("ready", () =>
-   console.log(`[${colors.FgCyan} Connected  ${colors.Reset}]\tLogged in as ${client.user.tag}!`)
-);
+client.on("ready", () => {
+   console.log(`[${colors.FgCyan} Connected  ${colors.Reset}]\tLogged in as ${client.user.tag}!`);
+
+   // Pour tout delete (normalement ça marche)
+   // client.application.commands.cache.each(cmd => {
+   //   console.log(cmd.name);
+   //   cmd.delete().then(cmd => console.log("deleted"));
+   // });
+   // client.guilds.cache.each(guild => {
+   //   console.log(guild.name);
+   //   guild.commands.cache.each(cmd =>{
+   //     console.log(cmd.name);
+   //     cmd.delete().then(cmd => console.log("deleted"));
+   //   });
+   // });
+});
