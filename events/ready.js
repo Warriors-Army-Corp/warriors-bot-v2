dcd990edef3c26b12bd143365f455bf671b6448c
Mizari-W <warriorofmeadow@gmail.com>
2021-12-24 13:53:47 +0100

    réglage d'un bug dans les logs de l'events ready

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const client = require("../index");
 const colors = require('../fonctions/colors.js');
 
 client.on("ready", () =>
-    console.log(`[${colors.FgCyan} Connected  ${colors.Reset}]\rLogged in as ${client.user.tag}!`)
+   console.log(`[${colors.FgCyan} Connected  ${colors.Reset}]\tLogged in as ${client.user.tag}!`)
 );
