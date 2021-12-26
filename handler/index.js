e8fa02eeb12e41890de0bacfe91c822c47638ae5
Mizari-W <warriorofmeadow@gmail.com>
2021-12-26 17:02:54 +0100

    les commandes sont forcément des slashcommands

--- handler/index.js
+++ handler/index.js
@@ -8,18 +8,6 @@ const globPromise = promisify(glob);
  * @param {Client} client
  */
 module.exports = async (client) => {
-    // Commands
-    const commandFiles = await globPromise(`${process.cwd()}/commands/**/*.js`);
-    commandFiles.map((value) => {
-        const file = require(value);
-        const splitted = value.split("/");
-        const directory = splitted[splitted.length - 2];
-
-        if (file.name) {
-            const properties = { directory, ...file };
-            client.commands.set(file.name, properties);
-        }
-    });
 
     // Events
     const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);
