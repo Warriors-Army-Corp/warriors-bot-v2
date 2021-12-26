3505e3ab084446c66b8e01b06ed8bf893dd0c9f2
Mizari-W <warriorofmeadow@gmail.com>
2021-12-26 17:31:33 +0100

    c'est le meilleur moyen que j'ai trouvé...

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,18 +3,11 @@ const client = require("../index");
 client.on("messageCreate", async (message) => {
     if (
         message.author.bot ||
-        !message.guild ||
-        !message.content.toLowerCase().startsWith(client.config.prefix)
+        !message.guild
     )
         return;
 
-    const [cmd, ...args] = message.content
-        .slice(client.config.prefix.length)
-        .trim()
-        .split(/ +/g);
-
-    const command = client.commands.get(cmd.toLowerCase()) || client.commands.find(c => c.aliases?.includes(cmd.toLowerCase()));
-
-    if (!command) return;
-    await command.run(client, message, args);
+    if (message.content === "/help"){
+      message.reply({content : "If you can't use slashcommands, please kick the bot and invite again with this link : https://discord.com/api/oauth2/authorize?client_id=591655828348731422&permissions=8&scope=bot%20applications.commands"});
+    }
 });
