87b1e647ffb1a1e092ebdf7a160266d6200c5d0f
Mizari-W <warriorofmeadow@gmail.com>
2021-12-30 02:58:16 +0100

    c'était le bordel mais on va rester sur ça

--- handler/index.js
+++ handler/index.js
@@ -1,6 +1,7 @@
 const { glob } = require("glob");
 const { promisify } = require("util");
 const { Client } = require("discord.js");
+const colors = require('../fonctions/colors.js');
 
 const globPromise = promisify(glob);
 
@@ -28,8 +29,29 @@ module.exports = async (client) => {
         arrayOfSlashCommands.push(file);
     });
     client.on("ready", async () => {
-        // Register for all guilds
-        await client.application
-            .commands.set(arrayOfSlashCommands);
+        // iter each guild where the client is
+        await client.guilds.cache.each(async guild => {
+          //await console.log(`[${colors.FgGreen}   Guild    ${colors.Reset}]\t${guild.name}`);
+          // for each guild, set all commands
+          await guild.commands.set(arrayOfSlashCommands);
+
+          // find all commands which is removed by default
+          // var commands = await guild.commands.cache.filter(cmd => cmd.defaultPermission === false);
+          // // iter all these commands
+          // await commands.each(async cmd => {
+          //   // logs
+          //   await console.log(`[${colors.FgYellow}  Command   ${colors.Reset}]\t⚠️ ${cmd.name} ⚠️`);
+          //
+          //   // set the perm data to all these commands
+          //   cmd.perm = await arrayOfSlashCommands.find(slashcmd => slashcmd.name === cmd.name).perm;
+          //   // find all roles with the perm needed by the command
+          //   var roles = await guild.roles.cache.filter(rl => rl.permissions.has(cmd.perm));
+          //   // iter all these roles
+          //   await roles.each(async role => {
+          //     // set the permission for these roles
+          //     await cmd.permissions.add({permissions: [{id: role.id, type: 'ROLE', permission: true}]}).then(() => console.log(`[${colors.FgGreen}    Done    ${colors.Reset}]\t✅ role : ${role.name}\n\t\trole id : ${role.id}\n\t\tcommand : ${cmd.name}\n\t\tguild : ${guild.name}`)).catch(err => console.log(`[${colors.FgRed}   Error    ${colors.Reset}]\t❌ role : ${role.name}\n\t\trole id : ${role.id}\n\t\tcommand : ${cmd.name}\n\t\tguild : ${guild.name}\n\t\terror : ${err}`));
+          //   });
+          // });
+        });
     });
 };
