2ba64749c3c11d454c461d504c6e83819aba07fe
Mizari-W <warriorofmeadow@gmail.com>
2022-09-07 22:17:37 +0200

    indentation

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,35 +1,37 @@
+const { ApplicationCommandOptionType } = require("discord.js");
+
 const client = require("../index");
 const colors = require('../fonctions/colors.js');
 
 client.on("interactionCreate", async (interaction) => {
-    // Slash Command Handling
-    if (interaction.isCommand()) {
-        await interaction.deferReply({ ephemeral: false }).catch(() => {});
+  // Slash Command Handling
+  if (interaction.isCommand()) {
+    await interaction.deferReply({ ephemeral: false }).catch(() => {});
 
-        const cmd = client.commandsFiles.get(interaction.commandName);
-        if (!cmd)
-            return interaction.followUp({ content: "An error has occured " });
+    const cmd = client.commandsFiles.get(interaction.commandName);
+    if (!cmd)
+      return interaction.followUp({ content: "An error has occured " });
 
-        const args = [];
+    const args = [];
 
-        for (let option of interaction.options.data) {
-            if (option.type === "SUB_COMMAND") {
-                if (option.name) args.push(option.name);
-                option.options?.forEach((x) => {
-                    if (x.value) args.push(x.value);
-                });
-            } else if (option.value) args.push(option.value);
-        }
-        interaction.member = interaction.guild.members.cache.get(interaction.user.id);
-
-        cmd.run(client, interaction, args);
-
-        // Logs
-        console.log(`[ ${colors.FgCyan}slash-cmd${colors.Reset}  ]\tauthor : ${interaction.user.username}\n\t\tguild : ${interaction.guild.name}\n\t\tcmd : ${interaction.commandName}\n\t\targs : ${args}`);
+    for (let option of interaction.options.data) {
+      if (option.type === ApplicationCommandOptionType.Subcommand) {
+        if (option.name) args.push(option.name);
+        option.options?.forEach((x) => {
+          if (x.value) args.push(x.value);
+        });
+      } else if (option.value) args.push(option.value);
     }
+    interaction.member = interaction.guild.members.cache.get(interaction.user.id);
 
-    // Context Menu Handling
-    if (interaction.isContextMenuCommand()) {
+    cmd.run(client, interaction, args);
+
+    // Logs
+    console.log(`[ ${colors.FgCyan}slash-cmd${colors.Reset}  ]\tauthor : ${interaction.user.username}\n\t\tguild : ${interaction.guild.name}\n\t\tcmd : ${interaction.commandName}\n\t\targs : ${args}`);
+  }
+
+  // Context Menu Handling
+  if (interaction.isContextMenuCommand()) {
         await interaction.deferReply({ ephemeral: false });
         const command = client.slashCommands.get(interaction.commandName);
         if (command) command.run(client, interaction);
