e33fca9ff22887cacf896d0206052ae5e376e1e8
Mizari-W <warriorofmeadow@gmail.com>
2022-09-22 17:39:03 +0200

    traduction cmd ban (fr - en)

--- Commands/slash/ban.js
+++ Commands/slash/ban.js
@@ -6,16 +6,21 @@ const { ApplicationCommandType, ApplicationCommandOptionType, PermissionsBitFiel
 module.exports = {
   name: "banhammer",
   description: "Ban a member",
+  descriptionLocalizations: {fr: "Ban un membre"},
   options: [
     {
-      name: "user",
-      description: "The user you want to ban",
+      name: "member",
+      nameLocalizations: {fr: "membre"},
+      description: "The member you want to ban",
+      descriptionLocalizations: {fr: "Le membre que vous voulez bannir"},
       type: ApplicationCommandOptionType.User,
       required: true
     },
     {
       name: "reason",
+      nameLocalizations: {fr: "raison"},
       description: "The reason of the ban",
+      descriptionLocalizations: {fr: "La raison du ban"},
       type: ApplicationCommandOptionType.String,
       required: false
     }
@@ -30,7 +35,7 @@ module.exports = {
    */
   run: async(client, interaction, args) => {
     // vérification que le bot a la perm de ban
-    if (interaction.guild.members.resolve(client.user).permissions.has("BAN_MEMBERS")) {
+    if (interaction.guild.members.resolve(client.user).permissions.has(PermissionsBitField.Flags.BanMembers)) {
       const userID = args[0]; // l'ID du membre à ban
       const mbr = interaction.guild.members.cache.get(userID); // le membre à ban
       // maintenant on vérifie que la victime est bannable
@@ -42,18 +47,30 @@ module.exports = {
         }
 
         // on envoit un message MP à la victime
-        await mbr.createDM().then(ch => ch.send(`Vous avez été banni de ${interaction.guild.name} par ${interaction.user.tag} pour la raison "${reason}"`)).catch();
+        await mbr.createDM().then(ch => {
+          if (interaction.locale === "fr") {
+            ch.send(`Vous avez été banni de ${interaction.guild.name} par ${interaction.user.tag} pour la raison "${reason}"`);
+          } else {
+            ch.send(`You've been banned from ${interaction.guild.name} by ${interaction.user.tag} for the reason "${reason}"`);
+          }
+        }).catch();
         // on indique que la personne s'est fait ban et on donne l'auteur du ban
-        await interaction.followUp({ content: `${mbr.user.tag} s'est pris un violent coup de banhammer sur la tête de la part de ${interaction.user}` });
+        await interaction.followUp({ content: interaction.locale === "fr"?
+          `${mbr.user.tag} s'est pris un violent coup de banhammer sur la tête de la part de ${interaction.user}`:
+          `${mbr.user.tag} took a violent banhammer hit on the head by ${interaction.user}`});
         // on ban la victime
         await mbr.ban({ reason: `"${reason}" par ${interaction.user.tag}` }).catch();
       // si la victime n'est pas bannable
       } else {
-        interaction.followUp({ content : "Je ne peux pas bannir cette personne" });
+        interaction.followUp({ content : interaction.locale === "fr"?
+          "Je ne peux pas bannir cette personne":
+          "I can't ban this member"});
       }
     // si le bot a pas la perm de ban
     } else {
-      interaction.followUp({ content: "J'ai pas la perm de ban..." });
+      interaction.followUp({ content: interaction.locale === "fr"?
+        "J'ai pas la perm de ban...":
+        "I don't have permission to ban..."});
     }
   }
 }
