a947a2ebbbc43544172f5785ae263fe630d37cf5
Mizari-W <warriorofmeadow@gmail.com>
2022-11-04 12:28:03 +0100

    webhook d'annonce pour les annonces de TPG

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,15 @@
 const client = require("../index");
+const { WebhookClient } = require("discord.js");
 
 client.on("messageCreate", async (message) => {
+
+    if (message.guild.id === "645239930896908293" && message.channel.id === "833710821833441360"){
+      if (message.author.id === "1035580588280774687" || message.author.id === "1035580650377461770"){
+        const webhook = new WebhookClient({id: "1038048951401713725", token: "WP7ucuABwc6TDOfulGPe_CTxXUfj5VpoxiWE-kwVZCOVxGtYZfwJfQdEfpwNu0TUe4Jd" });
+        webhook.send(message.content);
+      }
+    }
+
     if (
         message.author.bot ||
         !message.guild
